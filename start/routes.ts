182b3271b66bac356e057fac4a816ad05344b466
Tim <tk.tim.flanke@gmail.com>
2022-12-28 13:58:08 +0100

    add database sqlite and pg

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,14 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/health', async ({ response }: HttpContextContract) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
 })
