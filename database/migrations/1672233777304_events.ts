3caca93a224f3309e75820adbec74a12ead19e89
hulkoba <jacoba@neighbourhood.ie>
2023-01-11 14:55:51 +0100

    fix: autogenerate uuid

--- database/migrations/1672233777304_events.ts
+++ database/migrations/1672233777304_events.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id').primary()
+      table.uuid('id').primary().defaultTo(this.raw('uuid_generate_v4()'))
       table.string('title', 255).notNullable().unique()
       table.string('description', 800)
       table.string('cover')
--- database/migrations/1672245940322_venues.ts
+++ database/migrations/1672245940322_venues.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id').primary()
+      table.uuid('id').primary().defaultTo(this.raw('uuid_generate_v4()'))
       table.string('name', 200).notNullable().unique()
       table.string('description', 500)
       table.string('street', 200)
