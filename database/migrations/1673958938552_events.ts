a913c34b50137fcef280ac2d43ece1af43572601
Tim <tk.tim.flanke@gmail.com>
2023-01-20 01:09:17 +0100

    feat(db): update events columns

---
+++ database/migrations/1673958938552_events.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'events'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('headliner').notNullable()
+      table.string('support')
+      table.string('date').notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('headliner')
+      table.dropColumn('support')
+      table.dropColumn('date')
+    })
+  }
+}
