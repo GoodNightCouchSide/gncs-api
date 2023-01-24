2382a87673811e1aa653e6a2ba464dfbb0aba43b
Tim <tk.tim.flanke@gmail.com>
2023-01-24 23:50:35 +0100

    fix(role): change role name type

--- app/Models/Role.ts
+++ app/Models/Role.ts
@@ -1,6 +1,7 @@
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import Roles from 'App/Enums/Roles'
 
 export default class Role extends BaseModel {
   @column({ isPrimary: true })
-  public name: string
+  public name: Roles
 }
